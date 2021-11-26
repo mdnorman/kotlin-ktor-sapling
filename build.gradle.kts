a93dcccb13e6bc2e66c104fc6e218df9ab4aabca
Michael D. Norman <michael.norman@denormans.com>
2021-11-25 22:48:11 -0600

    chore: replace .+ dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ group = "CHANGEME"
 version = "0.0.1"
 
 val kotlinVersion = "1.3.61"
-val junitVersion = "5.4.+"
+val junitVersion = "5.4.0"
 
 plugins {
   kotlin("jvm") version "1.6.0"
@@ -26,12 +26,12 @@ repositories {
 }
 
 dependencies {
-  implementation("io.github.microutils:kotlin-logging:1.6.+")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.+")
-  implementation("org.slf4j:slf4j-simple:1.7.+")
+  implementation("io.github.microutils:kotlin-logging:1.6.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
+  implementation("org.slf4j:slf4j-simple:1.7.0")
   implementation(kotlin("reflect", kotlinVersion))
   implementation(kotlin("stdlib-jdk8", kotlinVersion))
-  testImplementation("com.natpryce:hamkrest:1.7.+")
+  testImplementation("com.natpryce:hamkrest:1.7.0")
   testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
   testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
